c29f6fcb9f06f0c29704901e82c83c6097e7e9fd
mfaaraz <muhammad.faraz.skipq@gmail.com>
2025-09-05 19:08:37 +0500

    testing42

--- seed-values.sh
+++ seed-values.sh
@@ -2,17 +2,16 @@
 set -euo pipefail
 
 DB_HOST=$1
-DB_PORT=$2
-DB_NAME=$3
-DB_USER=$4
-DB_PASS=$5
+DB_NAME=$2
+DB_USER=$3
+DB_PASS=$4
 
 sudo apt update -y
 sudo apt install -y postgresql postgresql-contrib
 
 PGPASSWORD="$DB_PASS" psql \
   -h "$DB_HOST" \
-  -p "$DB_PORT" \
+  -p 5432 \
   -U "$DB_USER" \
   -d "$DB_NAME" \
   -c "INSERT INTO items_item (name, description) VALUES
